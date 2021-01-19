701c9e4b09d44af5337c81e8c22fc9de95692cf3
Sean Murphy <sdmurphy.1999@gmail.com>
2021-01-18 22:10:04 -0800

    Added ranking stats storyboard

---
+++ TimeShare/View Controllers/RankingScreens/RankingStatsVC.swift
@@ -0,0 +1,26 @@
+//
+//  RankingStatsVC.swift
+//  TimeShare
+//
+//  Created by Sean Murphy on 1/18/21.
+//  Copyright © 2021 harrychuang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RankingStatsVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var currentRankLabel: UILabel!
+    @IBOutlet weak var nextRankLabel: UILabel!
+    @IBOutlet weak var rankPromotionInLabel: UILabel!
+    @IBOutlet weak var rankLevelLabel: UILabel!
+    @IBOutlet weak var averageHoursLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+    
+}
