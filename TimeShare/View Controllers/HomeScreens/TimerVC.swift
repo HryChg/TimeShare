e246012dc946358d17c151a411f1927889e816b6
Harry C <38871934+HryChg@users.noreply.github.com>
2021-02-08 08:22:44 -0800

    Styled Progress Ring

--- TimeShare/View Controllers/HomeScreens/TimerVC.swift
+++ TimeShare/View Controllers/HomeScreens/TimerVC.swift
@@ -19,18 +19,28 @@ class TimerVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        styleProgressRing()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
         // Somewhere not in viewDidLoad (since the views have not set yet, thus cannot be animated)
-        // Remember to use unowned or weak self if refrencing self to avoid retain cycle
-        progressRing.startProgress(to: 49, duration: 2.0) {
+        // Remember to use unowned or weak self if referencing self to avoid retain cycle
+        progressRing.startProgress(to: 100, duration: 2.0) {
             print("Done animating!")
-            // Do anything your heart desires...
         }
 
+    }
+
+    private func styleProgressRing() {
+        // Looked up in UICircularRing.swift
+        progressRing.startAngle = CGFloat(270)
+        progressRing.outerRingColor = UIColor.clear
+        progressRing.innerRingColor = UIColor(named: K.BrandColors.color6)!
+        progressRing.innerRingWidth = 10
+        progressRing.font = UIFont.systemFont(ofSize: 30)
+        progressRing.valueFormatter = TimerVCProgressRingFormatter(valueIndicator: "min left.");
     }
 }
 
@@ -46,5 +56,97 @@ extension TimerVC: UITableViewDataSource {
         cell.textLabel?.text = members[indexPath.row].name
         // TODO: Add more attributes
         return cell
+    }
+}
+
+class TimerVCProgressRingFormatter: UICircularRingValueFormatter{
+    // MARK: Members
+
+    /**
+     The name of the value indicator the value label will
+     appened to the value
+     Example: " GB" -> "100 GB"
+
+     ## Important ##
+     Default = "%"
+
+     ## Author
+     Luis Padron
+     */
+    public var valueIndicator: String
+
+    /**
+     A toggle for either placing the value indicator right or left to the value
+     Example: true -> "GB 100" (instead of 100 GB)
+
+     ## Important ##
+     Default = false (place value indicator to the right)
+
+     ## Author
+     Elad Hayun
+     */
+    public var rightToLeft: Bool
+
+    /**
+     A toggle for showing or hiding floating points from
+     the value in the value label
+
+     ## Important ##
+     Default = false (dont show)
+
+     To customize number of decmial places to show, assign a value to decimalPlaces.
+
+     ## Author
+     Luis Padron
+     */
+    public var showFloatingPoint: Bool
+
+    /**
+     The amount of decimal places to show in the value label
+
+     ## Important ##
+     Default = 2
+
+     Only used when showFloatingPoint = true
+
+     ## Author
+     Luis Padron
+     */
+    public var decimalPlaces: Int
+
+    // MARK: Init
+
+    public init(valueIndicator: String = "%",
+                rightToLeft: Bool = false,
+                showFloatingPoint: Bool = false,
+                decimalPlaces: Int = 2) {
+        self.valueIndicator = valueIndicator
+        self.rightToLeft = rightToLeft
+        self.showFloatingPoint = showFloatingPoint
+        self.decimalPlaces = decimalPlaces
+    }
+
+    // MARK: API
+
+    /// formats the value of the progress ring using the given properties
+    public func string(for value: Any) -> String? {
+        guard var value = value as? CGFloat else { return nil }
+        value = 100 - value // TODO Better Code. Reverse the value for string.
+
+        if rightToLeft {
+            if showFloatingPoint {
+                return "\(valueIndicator)" + String(format: "%.\(decimalPlaces)f", value)
+            } else {
+                return "\(valueIndicator)\(Int(value))"
+            }
+
+        } else {
+            if showFloatingPoint {
+                // TODO Fix this string
+                return String(format: "%.\(decimalPlaces)f", value) + "\(valueIndicator)"
+            } else {
+                return "\(Int(value))\(valueIndicator)"
+            }
+        }
     }
 }
